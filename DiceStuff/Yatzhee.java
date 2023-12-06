65389c45cb8d1374bf812a546c139aee6c0a7284
Jasper Gough <jasper.gough@mrjaffesclass.com>
2023-12-06 11:44:29 -0800

    yahtzee almost

--- DiceStuff/Yatzhee.java
+++ DiceStuff/Yatzhee.java
@@ -6,6 +6,8 @@ public class Yatzhee
     Die6 die3;
     Die6 die4;
     Die6 die5;
+    String toString;
+    String summarize;
     public Yatzhee()
     {
        die1 = new Die6();
@@ -25,5 +27,14 @@ public class Yatzhee
     public void roll(int dieNumber)
     {
         
+    }
+    public String summarize() {
+        summarize = "1-" + die1.getValue() + "; " + "2-" + die2.getValue() + "; " + "3-" + die3.getValue() + "; " + "4-" + die4.getValue() + "; " + "5-" + die5.getValue();
+        return summarize;
+    }
+    public String toString()
+    {
+        toString = "Dice values: " + die1.getValue() + " " + die2.getValue() + " " + die3.getValue() + " " + die4.getValue() + " " + die5.getValue();
+        return toString;
     }
 }
