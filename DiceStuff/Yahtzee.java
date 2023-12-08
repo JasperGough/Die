c8d5e342a86f0814390bf226e318075e933fda75
Jasper Gough <jasper.gough@mrjaffesclass.com>
2023-12-08 11:43:38 -0800

    almost yahtzee

--- DiceStuff/Yahtzee.java
+++ DiceStuff/Yahtzee.java
@@ -8,6 +8,7 @@ public class Yahtzee
     Die6 die5;
     private String toString;
     private String summarize;
+    private int[] rollArray = new int[5];
     public Yahtzee()
     {
        die1 = new Die6();
@@ -19,30 +20,41 @@ public class Yahtzee
     public void roll()
     {
         die1.roll();
+        rollArray[0] = die1.getValue();
         die2.roll();
+        rollArray[1] = die2.getValue();
         die3.roll();
+        rollArray[2] = die3.getValue();
         die4.roll();
+        rollArray[3] = die4.getValue();
         die5.roll();
+        rollArray[4] = die5.getValue();
     }
     public void roll(int dieNumber)
     {
         if (dieNumber == 1) {
             die1.roll();
+            rollArray[0] = die1.getValue();
         } else if (dieNumber == 2) {
             die2.roll();
+            rollArray[1] = die2.getValue();
         } else if (dieNumber == 3) {
             die3.roll();
+            rollArray[2] = die3.getValue();
         } else if (dieNumber == 4) {
             die4.roll();
+            rollArray[3] = die4.getValue();
         } else if (dieNumber == 5) {
             die5.roll();
+            rollArray[4] = die5.getValue();
         }
     }
     public String summarize() {
-        for (int i = 1; i <= 6; i++) {
-            
+        int [] tally = {0, 0, 0, 0, 0, 0};
+        for (int i = 0; i <= 4; i++) {
+            tally[rollArray[i]] += 1;
         }
-        return "idk yet lmao";
+        return "1-"+tally[0]+"; "+"2-"+tally[1]+"; "+"3-"+tally[2]+"; "+"4-"+tally[3]+"; "+"5-"+tally[4]+"; "+"6-"+tally[5];
     }
     public String toString()
     {
